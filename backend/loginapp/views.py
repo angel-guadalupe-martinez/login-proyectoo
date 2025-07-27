ed84693b9f23ea0ae9da60c129c30a8498ea8c4f
Angel1704Guadalupe <21302088@utfv.edu.mx>
2025-07-27 13:10:58 -0600

    Terminé Backend

---
+++ backend/loginapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+            password = data.get('password')
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                return JsonResponse({'status': 'ok'})
+            else:
+                return JsonResponse({'status': 'fail'}, status=401)
+        except json.JSONDecodeError:
+            return JsonResponse({'status': 'error', 'message': 'Invalid JSON'}, status=400)
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Only POST allowed'}, status=405)
+
